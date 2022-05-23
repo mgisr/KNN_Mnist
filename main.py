78c0ab744f0cbacf475797f0af5179305e744118
mgisr <a2509121896@gmail.com>
2022-05-23 09:13:59 +0800

    test

--- main.py
+++ main.py
@@ -6,14 +6,18 @@ from PIL import Image
 
 def main():
     (train_data, train_label), (test_data, test_label) = load_data()
+    # print(train_data.shape)
     train_data = train_data.reshape((train_data.shape[0], -1))
+    # print(train_data.shape)
     test_data = test_data.reshape((test_data.shape[0], -1))
     test_label = test_label.reshape((1, -1))
     tool = KNN()
     tool.fit(train_data, train_label)
-    img = Image.fromarray(test_data[1].reshape((28, 28)))
+    res = tool.predict(test_data[0:2])
+    img = Image.fromarray(test_data[0].reshape((28, 28)))
     plt.imshow(img)
     plt.show()
+    print(res[0])
 
 
 if __name__ == '__main__':
